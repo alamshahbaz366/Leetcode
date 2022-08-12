27c080abac5f87ce273bf093a6c9f0a1708fa161
Md Shahbaz Alam <88443812+alamshahbaz366@users.noreply.github.com>
2022-08-12 17:40:16 +0530

    Create 01-Count-Integers-With-Even-Digit-Sum.cpp

---
+++ 02-Solutions/01-Count-Integers-With-Even-Digit-Sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    // Sum of digits
+    int sumDigits(int num){
+        int sum = 0;
+        while(num!=0){
+        
+        sum = sum + num % 10;
+        num = num / 10;
+        } 
+        return sum;
+    }
+    
+    int countEven(int num) {
+        int ctr = 0;
+        for(int i=1; i<=num; i++){
+            if((sumDigits(i))%2 == 0)
+                ctr++;
+        }
+        
+
+    return ctr;    
+    }
+    
+};
